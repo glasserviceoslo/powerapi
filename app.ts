cd30ce929e4db71db1234284145a31df9f1b7680
Dovlat <dovlat93@gmail.com>
2022-08-07 14:13:51 +0400

    add app url to resolve cpanel url

--- app.ts
+++ app.ts
@@ -8,10 +8,10 @@ const app: Application = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (_req, res) =>
+app.get('/powerapi/', (_req, res) =>
   res.json({ message: 'Welcome to Aploskod integration API!' })
 );
-app.use('/oauth', poRoutes);
+app.use('/powerapi/oauth', poRoutes);
 
 app.use(errorHandler);
 app.use(globalErrorHandler);
