b32eea6e52cfe56d81b9dbb46a071306fabbfdf0
Dovlat Yakshibayev <30911739+davlet61@users.noreply.github.com>
2022-08-07 16:38:06 +0400

    dev (#1)
    * add workspace settings to gitignore
    
    * adds a cpanel.yml
    
    * remove .cpanel.yml. switch to npm
    
    * add lock file to gitignore
    
    * add app url to resolve cpanel url
    
    * cleanup tsconfig
    
    * minor refactoring
    
    * remove app url from requests in favor of subdomain
    
    * add versioning
    
    * changes folder structure in favor of versioning
    
    * adds workflow action to copy files to cpanel

--- app.ts
+++ app.ts
@@ -1,17 +1,16 @@
 import express, { Application } from 'express';
 import cors from 'cors';
-import poRoutes from './routes/poRoutes';
-import { errorHandler, globalErrorHandler } from './controllers/errorHandlers';
+import poRoutes from './v1/routes/poRoutes';
+import {
+  errorHandler,
+  globalErrorHandler,
+} from './v1/controllers/errorHandlers';
 
 const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
-
-app.get('/', (_req, res) =>
-  res.json({ message: 'Welcome to Aploskod integration API!' }),
-);
-app.use('/oauth', poRoutes);
+app.use('/', poRoutes);
 
 app.use(errorHandler);
 app.use(globalErrorHandler);
--- routes/poRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getAuthToken } from '../controllers/oauthToken';
-
-const router: Router = Router();
-
-router.post('/', getAuthToken);
-
-export default router;
