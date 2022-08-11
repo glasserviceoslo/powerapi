3511ce71f8a018bfd3ba7498570417abfa120529
Dovlat <dovlat93@gmail.com>
2022-08-11 16:21:36 +0400

    cleans up package/package-lock.json

--- app.ts
+++ app.ts
@@ -1,6 +1,5 @@
 import express, { Application } from 'express';
 import cors from 'cors';
-import session from 'express-session';
 import poRoutes from './v1/routes/poRoutes';
 import { errorHandler, globalErrorHandler } from './v1/controllers/errorHandlers';
 
@@ -8,14 +7,6 @@ const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(
-  session({
-    resave: true,
-    saveUninitialized: true,
-    secret: 'secret',
-    cookie: { secure: true },
-  }),
-);
 app.use('/', poRoutes);
 app.use(errorHandler);
 app.use(globalErrorHandler);
