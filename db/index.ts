40d0a0e8a24fec0ab1c372649b33421eb1a44e96
Dovlat <dovlat93@gmail.com>
2022-08-13 18:01:54 +0400

    adds db port to env vars

--- db/index.ts
+++ db/index.ts
@@ -2,7 +2,7 @@ import mysql from 'mysql2';
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: 8889,
+  port: parseInt(process.env.DB_PORT!, 10) || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
