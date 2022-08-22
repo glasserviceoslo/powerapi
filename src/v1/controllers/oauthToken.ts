5f6f17da6e198b7f03515fafc94eb5776408f577
davlet61 <dovlat@gmail.com>
2022-08-22 19:18:35 +0200

    adds relevant status code to auth route errors

--- src/v1/controllers/oauthToken.ts
+++ src/v1/controllers/oauthToken.ts
@@ -1,11 +1,14 @@
 import { NextFunction, Request, Response } from 'express';
+import { IError } from 'src/types';
 import { getTokens, getTokenWithRefresh, keysToBase64 } from 'src/v1/utils/accessToken';
 
 export const getAccessToken = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { application_key, client_key } = req.headers as { [key: string]: string };
     if (!application_key || !client_key) {
-      throw new Error('Missing Application Key or Client Key');
+      const err: IError = new Error('Missing Application Key or Client Key');
+      err.status = 400;
+      next(err);
     }
     const base64 = keysToBase64(application_key, client_key);
     const response = await getTokens(base64);
@@ -19,7 +22,9 @@ export const refreshAccessToken = async (req: Request, res: Response, next: Next
   try {
     const { refresh_token } = req.headers;
     if (!refresh_token) {
-      throw new Error('Missing Refresh Token in headers');
+      const err: IError = new Error('Missing Refresh Token in headers');
+      err.status = 400;
+      next(err);
     }
     const response = await getTokenWithRefresh(refresh_token as string);
     res.json(response);
