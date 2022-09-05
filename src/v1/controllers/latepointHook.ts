efe80ecf43f89262f1835c4823b0e3688b90c1c6
Dovlat <dovlat93@gmail.com>
2022-09-05 05:46:21 +0200

    adds suitecrm controller

--- src/v1/controllers/latepointHook.ts
+++ src/v1/controllers/latepointHook.ts
@@ -1,15 +1,38 @@
 import { NextFunction, Request, Response } from 'express';
-import { getTokens } from '../services/suiteRequests';
+import { createAccountModule, getFilteredCollection, getTokens, updateModule } from '../services/suiteRequests';
 
 export const moduleFromHook = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    // const { customer } = req.body;
+    const { customer } = req.body;
+    const { custom_fields } = customer;
     const { access_token } = await getTokens();
 
-    // ADD SUITE CREATE MODULE
+    const accountData = {
+      data: {
+        type: 'Accounts',
+        attributes: {
+          name: customer.full_name,
+          email1: customer.email,
+          phone_office: customer.phone,
+          phone_fax: custom_fields.cf_hOOzZCcn,
+          description: custom_fields.cf_nSrCbfGj,
+          billing_address_street: custom_fields.cf_ikbgaUAm,
+          billing_address_city: custom_fields.cf_a1uMDl3x,
+          billing_address_postalcode: custom_fields.cf_S7vs1QMZ,
+          billing_address_country: 'NORGE',
+        },
+      },
+    };
 
-    res.status(201).json(access_token);
+    const { data: existing } = await getFilteredCollection(access_token, customer.full_name, customer.email);
+    if (existing.length > 0) {
+      const newVal = { ...accountData, data: { ...accountData.data, id: existing.id } };
+      const updatedAccount = await updateModule(access_token, newVal);
+      return res.status(201).json(updatedAccount);
+    }
+    const account = await createAccountModule(access_token, accountData);
+    return res.status(201).json(account);
   } catch (error) {
-    next(error);
+    return next(error);
   }
 };
--- src/v1/services/suiteRequests.ts
+++ src/v1/services/suiteRequests.ts
@@ -1,13 +1,13 @@
 import axios from 'axios';
 import { ITokenResponse } from 'src/types';
 
-const { SUITE_TOKEN_URL, SUITE_CLIENT_ID, SUITE_CLIENT_SECRET } = process.env;
+const { SUITE_URL, SUITE_CLIENT_ID, SUITE_CLIENT_SECRET } = process.env;
 
 export const getTokens = async () => {
   const options = {
     method: 'POST',
-    url: '/Api/access_token',
-    baseURL: SUITE_TOKEN_URL,
+    url: '/access_token',
+    baseURL: SUITE_URL,
     headers: { 'content-type': 'application/x-www-form-urlencoded' },
     data: new URLSearchParams({
       grant_type: 'client_credentials',
@@ -24,14 +24,54 @@ export const createAccountModule = async (accessToken: string, args: any) => {
   const options = {
     method: 'POST',
     url: '/V8/module',
-    baseURL: SUITE_TOKEN_URL,
+    baseURL: SUITE_URL,
     headers: {
       'content-type': 'application/json',
       Authorization: `Bearer ${accessToken}`,
     },
     data: args,
   };
+  try {
+    const { data } = await axios.request(options);
+    return data;
+  } catch (error: any) {
+    return error.message;
+  }
+};
 
-  const { data } = await axios.request(options);
-  return data;
+export const updateModule = async (accessToken: string, args: any) => {
+  const options = {
+    method: 'PATH',
+    url: '/V8/module',
+    baseURL: SUITE_URL,
+    headers: {
+      'content-type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+    data: args,
+  };
+  try {
+    const { data } = await axios.request(options);
+    return data;
+  } catch (error: any) {
+    return error.message;
+  }
+};
+
+export const getFilteredCollection = async (accessToken: string, name: string, email: string) => {
+  const options = {
+    method: 'GET',
+    url: `/V8/module/Accounts?filter[name][eq]=${name}&filter[operator]=and&filter[email1][eq]=${email}`,
+    baseURL: SUITE_URL,
+    headers: {
+      'content-type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+  };
+  try {
+    const { data } = await axios.request(options);
+    return data;
+  } catch (error: any) {
+    return error.message;
+  }
 };
