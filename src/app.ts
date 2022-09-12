884edd055d627f6a94ef3feb1c1145ed7bcfa748
Dovlat <dovlat93@gmail.com>
2022-09-12 14:16:50 +0200

    minor change

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/v1', (_req, res) => {
-  console.log(`\x1b[33m[${new Date().toUTCString()}] \x1b[0m=>\x1b[32m The endpoint was hit\x1b[0m`);
+  console.log(`\x1b[33m[${new Date().toUTCString()}] \x1b[0m=>\x1b[32m The endpoint was hit!\x1b[0m`);
   res.json({ message: 'Welcome to Aploskod integration API!' });
 });
 
