88b51928f254ea9a99f844c208a47e09bca1d5f0
Dovlat <dovlat93@gmail.com>
2022-10-10 01:14:33 +0200

    adds address fields to svenn client

--- src/v1/webhooks/controllers/svenn.ts
+++ src/v1/webhooks/controllers/svenn.ts
@@ -6,6 +6,7 @@ import { createProject } from '$v1/requests/svenn/projects';
 export const latepointToSvenn = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { customer } = req.body;
+    const { custom_fields } = customer;
     const {
       data: { access_token },
     } = await getToken();
@@ -15,17 +16,33 @@ export const latepointToSvenn = async (req: Request, res: Response, next: NextFu
       contact_person: customer.full_name,
       email: customer.email,
       phone: customer.phone,
+      billing_address: {
+        country: 'NO',
+        type: 'billing',
+        street: custom_fields.cf_ikbgaUAm,
+        city: custom_fields.cf_a1uMDl3x,
+        state: null,
+        zip: custom_fields.cf_S7vs1QMZ,
+      },
+      shipping_address: {
+        country: 'NO',
+        type: 'shipping',
+        street: custom_fields.cf_ikbgaUAm,
+        city: custom_fields.cf_a1uMDl3x,
+        state: null,
+        zip: custom_fields.cf_S7vs1QMZ,
+      },
     };
 
     const client = await createClient(access_token, clientData);
 
     const projectData = {
       user_id: 20706,
-      name: customer.custom_fields.cf_ikbgaUAm,
-      address: `${customer.custom_fields.cf_ikbgaUAm}, ${customer.custom_fields.cf_a1uMDl3x}, Norge`,
+      name: custom_fields.cf_ikbgaUAm,
+      project_address: `${custom_fields.cf_ikbgaUAm}, ${custom_fields.cf_a1uMDl3x}, Norge`,
       start_date: req.body.start_datetime.split('T')[0],
       client_id: client.id,
-      description: customer.custom_fields.cf_nSrCbfGj,
+      description: custom_fields.cf_nSrCbfGj,
     };
 
     const project = await createProject(access_token, projectData);
