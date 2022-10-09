2ae19f6ceb5f2441d848cc4b7c093a60645baf8e
Dovlat <dovlat93@gmail.com>
2022-10-09 23:34:56 +0200

    adds svenn task route

---
+++ src/v1/requests/svenn/tasks.ts
@@ -0,0 +1,15 @@
+import { axiosRequest } from '$helpers';
+
+export const createTask = async (accessToken: string, args: any) => {
+  const options = {
+    method: 'POST',
+    url: '/task',
+    baseURL: process.env.SVENN_URL,
+    headers: {
+      'content-type': 'application/json; charset=utf-8',
+      Authorization: `Bearer ${accessToken}`,
+    },
+    data: args,
+  };
+  return axiosRequest(options);
+};
---
+++ src/v1/svenn/controllers/tasks.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from 'express';
+import { createTask } from '$v1/requests/svenn/tasks';
+
+export const createNew = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { access_token } = req.headers;
+    const task = await createTask(access_token, req.body);
+    res.status(201).json(task);
+  } catch (error) {
+    next(error);
+  }
+};
