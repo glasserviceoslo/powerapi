d8d3e045119294c8dbed39d337cccbe365b54ba7
Dovlat <dovlat93@gmail.com>
2023-04-23 13:12:00 +0200

    cleanup

--- src/app.ts
+++ src/app.ts
@@ -16,23 +16,14 @@ const app: Application = express();
 Sentry.init({
   dsn: 'https://19bd89163958457e9c1aedf8edfa69e4@o4504951835983872.ingest.sentry.io/4505062670729216',
   integrations: [
-    // enable HTTP calls tracing
     new Sentry.Integrations.Http({ tracing: true }),
-    // enable Express.js middleware tracing
     new Sentry.Integrations.Express({ app }),
-    // Automatically instrument Node.js libraries and frameworks
     ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
   ],
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
   tracesSampleRate: 1.0,
 });
 
-// RequestHandler creates a separate execution context, so that all
-// transactions/spans/breadcrumbs are isolated across requests
 app.use(Sentry.Handlers.requestHandler());
-// TracingHandler creates a trace for every incoming request
 app.use(Sentry.Handlers.tracingHandler());
 
 app.use(cors());
